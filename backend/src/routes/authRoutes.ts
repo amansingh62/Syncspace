111dcc7a41d5c53fb209c1f3c470f2d1db162db0
Aman Singh <coolaman6299@gmail.com>
2026-01-18 02:13:31 +0530

    Included the logout all route too

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { login, register, logout } from "../controllers/authController.js";
+import { login, register, logout, logoutAll } from "../controllers/authController.js";
 import { validate } from "../middlewares/validate.js";
 import { loginSchema, registerSchema } from "../validate/authSchema.js";
-
+import { requireAuth } from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.post("/register", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
 router.post("/logout", logout);
+router.post("/logout-all", requireAuth, logoutAll);
 
 export default router;
