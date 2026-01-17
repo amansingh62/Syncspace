c1442b8693c063b6d3440fb5f2132a2569322031
Aman Singh <coolaman6299@gmail.com>
2026-01-17 22:35:57 +0530

    Added configuration for the database and written usable components for cookies and tokens

---
+++ backend/src/config/env.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const env = {
+    port: process.env.PORT || 5000,
+    ACCESS_SECRET: process.env.ACCESS_TOKEN_SECRET!,
+    REFRESH_SECRET: process.env.REFRESH_TOKEN_SECRET!
+};
---
+++ backend/src/utilities/token.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
+
+export const signAccessToken = (userId : string) =>
+  jwt.sign({ userId }, env.ACCESS_SECRET, { expiresIn: "15m"});
+
+export const signRefreshToken = (userId : string) => 
+    jwt.sign({ userId }, env.REFRESH_SECRET, { expiresIn: "7d"});
+
+export const verifyAccessToken = (token : string) =>
+    jwt.verify(token, env.ACCESS_SECRET);
+
+export const verifyRefreshToken = (token : string) =>
+    jwt.verify(token, env.REFRESH_SECRET);
