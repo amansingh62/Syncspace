d8dd6463d2d56e0630e17de00d56cb3d67ec1359
Aman Singh <coolaman6299@gmail.com>
2026-01-18 23:27:18 +0530

    Added logic for the login form

---
+++ frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+export default function LoginPage() {
+    const router = useRouter();
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const submit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const data = { email, password };
+
+       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type":"application/json"},
+        body: JSON.stringify(data),
+        credentials: "include"
+       });
+
+       if(res.ok){
+        router.push("/app");
+       }
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+            <button>Login</button>
+        </form>
+    )
+}
