e5f87dbeb5cc3088b319b94915b61f784495c428
Aman Singh <coolaman6299@gmail.com>
2026-02-05 23:20:54 +0530

    Changed cookies settings for production

--- backend/src/utilities/cookies.ts
+++ backend/src/utilities/cookies.ts
@@ -3,22 +3,31 @@ import type { Response } from "express";
 export const setAuthCookies = (res: Response, accessToken: string, refreshToken: string) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 15 * 60 * 1000,
         path: "/", 
     });
 
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/", 
     });
 };
 
 export const clearAuthCookies = (res: Response) => {
-  res.clearCookie("accessToken");
-  res.clearCookie("refreshToken");
+  res.clearCookie("accessToken", {
+  path: "/",
+  sameSite: "none",
+  secure: true,
+});
+
+res.clearCookie("refreshToken", {
+  path: "/",
+  sameSite: "none",
+  secure: true,
+});
 };
